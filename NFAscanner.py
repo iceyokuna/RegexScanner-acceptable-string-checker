77f2e0db6dc4d23e93803b965971b4c977c060d9
Iceyo Kuna <iceyokuna@hotmail.com>
2019-02-26 01:24:48 +0700

    [DONE] all operation

--- NFAscanner.py
+++ NFAscanner.py
@@ -20,7 +20,7 @@ class FA:
 
 
 def concat(fa, char):
-    out_fa = FA(fa.Q, fa.Sigma, fa.delta , fa.q0, fa.F)
+    out_fa = fa.copy()
     new_state = len(out_fa.Q)
     out_fa.Q.add(new_state)
     out_fa.Sigma.add(char)
@@ -30,43 +30,48 @@ def concat(fa, char):
 
 
 def star(fa):
-    out_fa = FA(fa.Q, fa.Sigma, fa.delta , fa.q0, fa.F)
-    new_start_state = len(FA.Q)
-    new_end_state = len(FA.Q) + 1
+    out_fa = fa.copy()
+    new_start_state = len(out_fa.Q)
+    new_end_state = len(out_fa.Q) + 1
 
     #set transition
-    FA.delta[(new_start_state,"")] = {FA.q0, new_end_state}
-    FA.delta[(list(FA.F)[0],"")] = {FA.q0, new_end_state}
+    out_fa.delta[(new_start_state,"")] = {out_fa.q0, new_end_state}
+    out_fa.delta[(list(out_fa.F)[0],"")] = {out_fa.q0, new_end_state}
 
     #update Q,q0,F
-    FA.Q.add(new_start_state)
-    FA.Q.add(new_end_state)
-    FA.q0 = new_start_state
-    FA.F = {new_end_state}
-    return FA
+    out_fa.Q.add(new_start_state)
+    out_fa.Q.add(new_end_state)
+    out_fa.q0 = new_start_state
+    out_fa.F = {new_end_state}
+    return out_fa
 
-def join(FA,subFA):
-    #re name subFA 
-    num_state = len(FA.Q)
+def join(mainFA,joinFA):
+    #re name subFA
+    fa = mainFA.copy()
+    subFA = joinFA.copy()
+    num_state = len(fa.Q)
     subFA.Q = {i+num_state for i in subFA.Q}
     new_delta = {}
-    
+
+    #update delta of subFA
     for i in subFA.delta:
         new_delta[(i[0] + num_state, i[1])] = {list(subFA.delta[i])[0] + num_state}
     subFA.delta = new_delta
-    
+    #update all attr of SubFA
     subFA.q0 += num_state
     subFA.F = {list(subFA.F)[0] + num_state}
-    FA.Sigma = FA.Sigma | subFA.Sigma
-    FA.Q = FA.Q | subFA.Q
+
+    #join sigma and Q
+    fa.Sigma = fa.Sigma | subFA.Sigma
+    fa.Q = fa.Q | subFA.Q
 
     #connect transition
-    FA.delta[(list(FA.F)[0], '')] = subFA.q0
-    FA.delta = {**FA.delta, **subFA.delta}
-    FA.F = subFA.F
-    return FA
+    fa.delta[(list(fa.F)[0], '')] = {subFA.q0} #add empty move to join 2 FA
+    fa.delta = {**fa.delta, **subFA.delta} #combine dict
+    fa.F = subFA.F #set new end
+    return fa
 
-def exclusiveOr(FA, expr):
+def exclusiveOr(expr):
     exclusiveList = []
     temp = []
     for char in expr:
@@ -76,9 +81,50 @@ def exclusiveOr(FA, expr):
             continue
         temp.append(char)
     exclusiveList.append(temp)
-    print(exclusiveList)
+
     #build FA in each elements in list
-    #merge to 1 fa
+    faList = []
+    for expr in exclusiveList:
+        faList.append(exprToFA(expr))
+
+    counter = 1
+    list_end_subFA = []
+    fa = FA({0}, set(), {} , 0, {0})
+    fa.delta[(fa.q0, '')] = set() #to prevent key error
+    
+    for sub_fa in faList:
+        subFA = sub_fa.copy()
+        subFA.Q = {i+counter for i in subFA.Q}
+        new_delta = {}
+        
+        #update delta of subFA
+        for i in subFA.delta:
+            new_delta[(i[0] + counter, i[1])] = {list(subFA.delta[i])[0] + counter}
+        subFA.delta = new_delta
+        
+        #update all attr of SubFA
+        subFA.q0 += counter
+        subFA.F = {list(subFA.F)[0] + counter}
+        
+        #join sigma and Q
+        fa.Sigma = fa.Sigma | subFA.Sigma
+        fa.Q = fa.Q | subFA.Q
+
+        #connect transition
+        fa.delta[(fa.q0, '')].add(subFA.q0) #add empty move to join 2 FA
+        fa.delta = {**fa.delta, **subFA.delta} #combine dict
+        list_end_subFA.append(list(subFA.F)[0]) #keep end state(s) 
+
+        #update counter
+        counter = len(fa.Q)
+    #connect to same end state
+    fa.Q.add(counter)
+    for end in list_end_subFA: #set new end state
+        fa.delta[(end,'')] = {counter}
+    fa.F = {counter}
+
+    return fa
+
 
 #simple expr to FA (convert sub expr to sub fa)
 def exprToFA(expr_list):
@@ -95,35 +141,37 @@ def exprToFA(expr_list):
             fa = concat(fa, char)
     return fa
     
-#TEST_CONCAT
+#########################TEST_CONCAT
 ##expr = ['a','a','b']
 ##fa = FA()
 ##for char in expr:
 ##    concat(fa, char)
 ##fa.show()
 
-#TEST_STAR *
+#########################TEST_STAR *
 ##fa = FA({0, 1, 2, 3}, {'a', 'b'}, {(0, 'a'): {1}, (1, 'a'): {2}, (2, 'b'): {3}}, 0, {3})
 ##star(fa)
 ##fa.show()
 
-#TEST_JOIN
+#########################TEST_JOIN
 ##expr = ['a','a','b']
 ##expr2 = ['a']
 ##fa = FA({0}, set(), {} , 0, {0})
 ##fa2 = FA({0}, set(), {} , 0, {0})
 ##
 ##for char in expr:
-##    concat(fa, char)
+##    fa = concat(fa, char)
 ##for char in expr2:
-##    concat(fa2, char)
-##    
+##    fa2 = concat(fa2, char)
+##fa.show()
+##fa2.show()
+##
 ##fa = join(fa,fa2)
 ##
 ##fa.show()
 
 
-#TEST ALL 1
+###########################TEST ALL 1
 ##expr = ['a','b']
 ##expr2 = ['c','c']
 ##
@@ -138,7 +186,7 @@ def exprToFA(expr_list):
 ##    
 ##fa.show()
 
-#TEST ALL 2
+###########################TEST ALL 2
 ##expr = ['a','b']
 ##expr2 = ['c','c']
 ##
@@ -156,17 +204,35 @@ def exprToFA(expr_list):
 ##    
 ##fa.show()
 
-#TEST ExclusiveOR (tokenize)
-##fa = FA({0}, set(), {} , 0, {0})
+###########################TEST ExclusiveOR (tokenize)
 ##expr = ['a','s','|','b','c','|','c','c']
-##exclusiveOr(fa, expr)
-
-#TEST construct FA from sub expr
-expr = ['a','b','c']
-fa = exprToFA(expr)
-fa.show()
-fa2 = exprToFA([fa,'*'])
-fa2.show()
-fa.show()
+##exclusiveOr(expr)
 
 
+###########################TEST construct FA from sub expr -> (abc)* #correct
+##expr = ['a','b','c']
+##fa = exprToFA(expr)
+##fa.show()
+##fa2 = exprToFA([fa,'*'])
+##fa2.show()
+
+
+###########################TEST construct FA from sub expr -> (ab)(cc) #correct
+##expr = ['a','b']
+##expr2 = ['c','c']
+##fa = exprToFA(expr)
+##fa.show()
+##fa2 = exprToFA(expr2)
+##fa2.show()
+##fa3 = exprToFA([fa,fa2])
+###((ab)(cc))*
+##fa3.show()
+##fa4 = exprToFA([fa3,'*'])
+##fa4.show()
+
+
+###########################TEST ExclusiveOR (aa|bb|)
+##fa = exprToFA(['a'])
+##expr = ['a','a','|','b','b']
+##out_fa = exclusiveOr(expr)
+##out_fa.show()
