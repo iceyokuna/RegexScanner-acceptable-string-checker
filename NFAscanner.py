a589e80f2442938cca32cc4743094d3b3ce85c42
Iceyo Kuna <iceyokuna@hotmail.com>
2019-02-26 02:31:34 +0700

    [DEV] debug regex to fa

--- NFAscanner.py
+++ NFAscanner.py
@@ -18,6 +18,26 @@ class FA:
         print("F = ", self.F)
         print('-----------------------------\n')
 
+class Stack:
+    def __init__(self):
+        self.elements = []
+        self.size = 0
+
+    def push(self, element):
+        self.size += 1
+        self.elements.append(element)
+
+    def pop(self):
+        self.size -= 1
+        return self.elements.pop()
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        return str(self.elements)
+
+
 
 def concat(fa, char):
     out_fa = fa.copy()
@@ -140,6 +160,67 @@ def exprToFA(expr_list):
         else:
             fa = concat(fa, char)
     return fa
+
+
+# get NFA from regex_expr
+def getFA(regex_expr):
+    regex_expr = '('+ regex_expr + ')'
+    print("original regex  :  " + regex_expr)
+
+    #pre process (to mange * ,change char* to (char)*)
+    temp_regex_expr = ""
+    for i in range(len(regex_expr)):
+        if(regex_expr[i] == '*' and regex_expr[i-1] != ')'):
+            temp_regex_expr = temp_regex_expr[0:-1]
+            temp_regex_expr += '(' + regex_expr[i-1] +')*'
+        else:
+            temp_regex_expr += regex_expr[i]
+    regex_expr = temp_regex_expr
+    print("Pre-process regex  :  " + regex_expr)
+    
+    #change regex to list of char in order to insert sub FA (FA object references)
+    expr = list(regex_expr) #read char by char O(n)
+
+    #dict of sub FA references
+    sub_fa = {}
+
+    stack = Stack()
+    sub_num = 1 #sub number postfix reference
+    index = 1
+    stack.push(expr[0]) #push '('
+    expr_temp = []
+    char = ''
+    root_ref = '' #when process is end, this variable will be the root FA reference(whole NFA)
+
+    while(len(stack) != 0):
+        if(expr[index] == ')'):  #case  ')'
+            char = stack.pop()
+            while(char != '('):
+                expr_temp = [char] + expr_temp
+                char = stack.pop()
+                
+            sub_fa["FA(" + str(sub_num) + ')'] = expr_temp
+            root_ref = "FA(" + str(sub_num) + ')'
+            if(len(stack) != 0):
+                stack.push(root_ref)
+            print(expr_temp)
+            expr_temp = []
+            sub_num += 1
+
+        elif(expr[index] == '*'): #case '*'
+            char = stack.pop()
+            expr_temp = [char] + expr_temp + ['*']
+            sub_fa["FA(" + str(sub_num) + ')'] = expr_temp
+            root_ref = "FA(" + str(sub_num) + ')'
+            stack.push(root_ref)
+            print(expr_temp)
+            expr_temp = []
+            sub_num += 1
+            
+        else:
+            stack.push(expr[index])
+        index += 1
+    return sub_fa
     
 #########################TEST_CONCAT
 ##expr = ['a','a','b']
@@ -231,22 +312,31 @@ def exprToFA(expr_list):
 ##fa4.show()
 
 
-###########################TEST ExclusiveOR (aa|bb|)
+###########################TEST ExclusiveOR (aa|bb|)  #correct
 ##fa = exprToFA(['a'])
 ##expr = ['a','a','|','b','b']
 ##out_fa = exclusiveOr(expr)
 ##out_fa.show()
 
-###########################TEST ExclusiveOR (12*2)
-expr = ['2']
-fa = exprToFA(expr)
-fa.show()
+###########################TEST ExclusiveOR (12*2)  #correct
+##expr = ['2']
+##fa = exprToFA(expr)
+##fa.show()
+##
+##expr2 = [fa,'*']
+##fa2 = exprToFA(expr2)
+##fa2.show()
+##
+##expr3 = ['1',fa2,'2']
+##fa3 = exprToFA(expr3)
+##fa3.show()
 
-expr2 = [fa,'*']
-fa2 = exprToFA(expr2)
-fa2.show()
-
-expr3 = ['1',fa2,'2']
-fa3 = exprToFA(expr3)
-fa3.show()
-
+###########################REGEX
+regex_expr = "12*2"
+#    regex_expr = "1(12*3|11)12(14)*"
+#    regex_expr = "(0|1|2|3|4|5|6|7|8|9)|(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"
+#    regex_expr = "(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*.|.(0|1|2|3|4|5|6||8|9)(0|1|2|3|4|5|6|7|8|9)*|(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*.(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"
+#    regex_expr = "(123)12"
+#    regex_expr = "(12*333)*5(1|2)"
+fa = getFA(regex_expr)
+print(fa)
